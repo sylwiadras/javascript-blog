44f320cde6875bc337371c8bf57dcbd93aa4ae38
Sylwia Dras <sylwiadras99@gmail.com>
2019-03-02 20:11:57 +0100

    Find correct article using selector

--- js/script.js
+++ js/script.js
@@ -27,12 +27,15 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
     }
   
-    /* [IN PROGRESS] get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
   
     /* add class 'active' to the correct article */
   }
